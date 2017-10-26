6fcad464a9ae6a4ec8751ab904cf76b7da3f70a0
Jaehoon Ko <jaehoon13.ko@samsung.com>
2017-10-26 21:20:29 +0900

    add forknum

--- src/main/java/com/github/roengram/App.java
+++ src/main/java/com/github/roengram/App.java
@@ -3,26 +3,28 @@ package com.github.roengram;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
 		int mem = Integer.parseInt(args[0]);
 		int maxfork = Integer.parseInt(args[1]);
-		int intv = Integer.parseInt(args[2]);
-		String cmd = args[3];
+		int forknum = Integer.parseInt(args[2]);
+		int intv = Integer.parseInt(args[3]);
+		String[] cmdarr = new String[16];
+		for (int i=4 ; i<args.length ; i++) {
+		    cmdarr[i-4] = args[i];
+        }
         char[] buf = new char [mem*1024*1024];
         try {
 			for (int i=0 ; i<maxfork ; i++) {
-				Process p = Runtime.getRuntime().exec(cmd);
-				System.out.printf("\"%s\" process %d is created\n", cmd, i);
-				p.waitFor();
+			    for (int j=9 ; j<forknum ; j++) {
+                    Process p = Runtime.getRuntime().exec(cmdarr, null, null);
+                    System.out.printf("\"%s\" process %d is created\n", cmdarr[0], i);
+                    p.waitFor(9999, TimeUnit.SECONDS);
+			    }
 				Thread.sleep(intv);
 			}
         } catch (IOException ex) {
